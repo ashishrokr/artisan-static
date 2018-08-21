77d826358b7d8398b604945fa045f5006204aee4
Ranie Santos <ranietots32@gmail.com>
2018-08-21 16:00:38 +0800

    Setup the template

--- config.php
+++ config.php
@@ -1,7 +1,48 @@
 <?php
 
 return [
-    'baseUrl' => '',
     'production' => false,
-    'collections' => [],
+    'baseUrl' => 'https://johndoe.netlify.com',
+    'site' => [
+        'title' => 'My Jigsaw Blog',
+        'description' => 'Personal blog of John Doe.',
+        'image' => 'share-image.png',
+    ],
+    'owner' => [
+        'name' => 'John Doe',
+        'twitter' => 'johndoe',
+        'github' => 'johndoe',
+    ],
+    'services' => [
+        'analytics' => 'UA-XXXXX-Y',
+        'disqus' => 'johndoe',
+        'cloudinary' => 'johndoe',
+        'jumprock' => 'johndoe',
+    ],
+    'collections' => [
+        'posts' => [
+            'path' => 'posts/{filename}',
+            'sort' => '-date',
+            'extends' => '_layouts.post',
+            'section' => 'postContent',
+            'isPost' => true,
+            'comments' => true,
+        ],
+        'tags' => [
+            'path' => 'tags/{filename}',
+            'extends' => '_layouts.tag',
+            'section' => '',
+            'name' => function ($page) {
+                return $page->getFilename();
+            },
+        ],
+    ],
+    'excerpt' => function ($page, $limit = 250, $end = '...') {
+        return $page->isPost
+            ? str_limit_soft(content_sanitize($page->getContent()), $limit, $end)
+            : null;
+    },
+    'imageCdn' => function ($page, $path) {
+        return "https://res.cloudinary.com/{$page->services->cloudinary}/image/upload/{$path}";
+    },
 ];
---
+++ source/_layouts/post.blade.php
@@ -0,0 +1,31 @@
+@extends('_layouts.master')
+
+@section('title', $page->title)
+
+@section('content')
+    <h1>{{ $page->title }}</h1>
+
+    <div>
+        <strong>{{ date('F j, Y', $page->date) }}</strong>
+        <p>
+            @foreach ($page->tags as $tag)
+                <a href="/tags/{{ $tag }}">{{ $tag }}</a>
+                {{ $loop->last ? '' : '-' }}
+            @endforeach
+        </p>
+    </div>
+
+    <p><em>DISCLAIMER: Any 3rd-party services in these posts and in the config are only recommendations/suggestions. I am not affiliated with any of them.</em></p>
+
+    <hr>
+
+    @yield('postContent')
+
+    <hr>
+
+    @if ($page->comments)
+        @include('_partials.disqus')
+    @else
+        <p>Comments are not enabled for this post.</p>
+    @endif
+@endsection
